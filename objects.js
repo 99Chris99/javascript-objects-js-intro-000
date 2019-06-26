c6d269ca5de8daf9c27daa91c284d1b45f3eb810
Chris Hodge <chrishodge87@gmail.com>
2019-06-26 16:10:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,8 @@
 var playlist = {
   "Bruce Springsteen": "Dancing in the Dark", "Alantic City"
 }
+
+/*
 function updatePlaylist(playlist, artist, song){
   return Object.assign({},playlist { artist: ['song']})
 }
