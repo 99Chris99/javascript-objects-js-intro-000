0c514406b8c3ce866a88fa5655681c2818747c0f
Chris Hodge <chrishodge87@gmail.com>
2019-06-26 16:09:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,11 @@
 var playlist = {
   "Bruce Springsteen": "Dancing in the Dark", "Alantic City"
 }
-function updatePlaylist(playlist,artist,song){
+function updatePlaylist(playlist, artist, song){
   return Object.assign({},playlist { artist: ['song']})
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist;
+  return playlist
 }
